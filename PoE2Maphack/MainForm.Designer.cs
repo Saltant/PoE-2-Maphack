d9e4e0dea7ad9fbb3c8fcc6176aea92b6b42e77d
Saltant <admin@saltant.io>
2025-10-16 20:04:59 +0300

    Fixed AOB Error

--- PoE2Maphack/MainForm.Designer.cs
+++ PoE2Maphack/MainForm.Designer.cs
@@ -55,10 +55,11 @@ partial class MainForm
         // 
         flowLayoutPanel1.Controls.Add(linkLabel1);
         flowLayoutPanel1.Dock = DockStyle.Bottom;
-        flowLayoutPanel1.Location = new Point(0, 75);
+        flowLayoutPanel1.Location = new Point(0, 109);
+        flowLayoutPanel1.Margin = new Padding(3, 4, 3, 4);
         flowLayoutPanel1.Name = "flowLayoutPanel1";
-        flowLayoutPanel1.Padding = new Padding(75, 3, 0, 0);
-        flowLayoutPanel1.Size = new Size(271, 23);
+        flowLayoutPanel1.Padding = new Padding(86, 4, 0, 0);
+        flowLayoutPanel1.Size = new Size(343, 31);
         flowLayoutPanel1.TabIndex = 0;
         // 
         // linkLabel1
@@ -67,9 +68,9 @@ partial class MainForm
         linkLabel1.Cursor = Cursors.Hand;
         linkLabel1.Font = new Font("Segoe UI", 9F);
         linkLabel1.LinkBehavior = LinkBehavior.HoverUnderline;
-        linkLabel1.Location = new Point(78, 3);
+        linkLabel1.Location = new Point(89, 4);
         linkLabel1.Name = "linkLabel1";
-        linkLabel1.Size = new Size(113, 15);
+        linkLabel1.Size = new Size(143, 20);
         linkLabel1.TabIndex = 0;
         linkLabel1.TabStop = true;
         linkLabel1.Text = "Available on GitHub";
@@ -85,10 +86,11 @@ partial class MainForm
         tableLayoutPanel1.Controls.Add(tableLayoutPanel2, 0, 0);
         tableLayoutPanel1.Dock = DockStyle.Fill;
         tableLayoutPanel1.Location = new Point(0, 0);
+        tableLayoutPanel1.Margin = new Padding(3, 4, 3, 4);
         tableLayoutPanel1.Name = "tableLayoutPanel1";
         tableLayoutPanel1.RowCount = 1;
         tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-        tableLayoutPanel1.Size = new Size(271, 75);
+        tableLayoutPanel1.Size = new Size(343, 109);
         tableLayoutPanel1.TabIndex = 1;
         // 
         // panel1
@@ -97,17 +99,19 @@ partial class MainForm
         panel1.Controls.Add(deactivateBtn);
         panel1.Controls.Add(activateBtn);
         panel1.Dock = DockStyle.Fill;
-        panel1.Location = new Point(138, 3);
+        panel1.Location = new Point(174, 4);
+        panel1.Margin = new Padding(3, 4, 3, 4);
         panel1.Name = "panel1";
-        panel1.Size = new Size(130, 69);
+        panel1.Size = new Size(166, 101);
         panel1.TabIndex = 0;
         // 
         // deactivateBtn
         // 
         deactivateBtn.AutoSize = true;
-        deactivateBtn.Location = new Point(16, 37);
+        deactivateBtn.Location = new Point(18, 49);
+        deactivateBtn.Margin = new Padding(3, 4, 3, 4);
         deactivateBtn.Name = "deactivateBtn";
-        deactivateBtn.Size = new Size(103, 19);
+        deactivateBtn.Size = new Size(129, 24);
         deactivateBtn.TabIndex = 1;
         deactivateBtn.TabStop = true;
         deactivateBtn.Text = "Deactivate MH";
@@ -116,9 +120,10 @@ partial class MainForm
         // activateBtn
         // 
         activateBtn.AutoSize = true;
-        activateBtn.Location = new Point(16, 12);
+        activateBtn.Location = new Point(18, 16);
+        activateBtn.Margin = new Padding(3, 4, 3, 4);
         activateBtn.Name = "activateBtn";
-        activateBtn.Size = new Size(91, 19);
+        activateBtn.Size = new Size(112, 24);
         activateBtn.TabIndex = 0;
         activateBtn.TabStop = true;
         activateBtn.Text = "Activate MH";
@@ -130,12 +135,13 @@ partial class MainForm
         tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
         tableLayoutPanel2.Controls.Add(doWorkBtn, 0, 1);
         tableLayoutPanel2.Controls.Add(statusLabel, 0, 0);
-        tableLayoutPanel2.Location = new Point(3, 3);
+        tableLayoutPanel2.Location = new Point(3, 4);
+        tableLayoutPanel2.Margin = new Padding(3, 4, 3, 4);
         tableLayoutPanel2.Name = "tableLayoutPanel2";
         tableLayoutPanel2.RowCount = 2;
         tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 60F));
         tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 40F));
-        tableLayoutPanel2.Size = new Size(129, 68);
+        tableLayoutPanel2.Size = new Size(147, 91);
         tableLayoutPanel2.TabIndex = 1;
         // 
         // doWorkBtn
@@ -144,10 +150,10 @@ partial class MainForm
         doWorkBtn.Dock = DockStyle.Fill;
         doWorkBtn.FlatStyle = FlatStyle.Flat;
         doWorkBtn.Font = new Font("Segoe UI", 9F);
-        doWorkBtn.Location = new Point(3, 40);
-        doWorkBtn.Margin = new Padding(3, 0, 3, 3);
+        doWorkBtn.Location = new Point(3, 54);
+        doWorkBtn.Margin = new Padding(3, 0, 3, 4);
         doWorkBtn.Name = "doWorkBtn";
-        doWorkBtn.Size = new Size(123, 25);
+        doWorkBtn.Size = new Size(141, 33);
         doWorkBtn.TabIndex = 0;
         doWorkBtn.Text = "Change State";
         doWorkBtn.TextAlign = ContentAlignment.TopCenter;
@@ -161,7 +167,7 @@ partial class MainForm
         statusLabel.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
         statusLabel.Location = new Point(3, 0);
         statusLabel.Name = "statusLabel";
-        statusLabel.Size = new Size(123, 40);
+        statusLabel.Size = new Size(141, 54);
         statusLabel.TabIndex = 1;
         statusLabel.Text = "Status: Deactivated";
         statusLabel.TextAlign = ContentAlignment.MiddleCenter;
@@ -175,40 +181,42 @@ partial class MainForm
         // 
         // contextMenuStrip1
         // 
+        contextMenuStrip1.ImageScalingSize = new Size(20, 20);
         contextMenuStrip1.Items.AddRange(new ToolStripItem[] { toolStripMenuItem1, toolStripMenuItem2, closeToolStripMenuItem });
         contextMenuStrip1.Name = "contextMenuStrip1";
-        contextMenuStrip1.Size = new Size(153, 70);
+        contextMenuStrip1.Size = new Size(178, 76);
         contextMenuStrip1.Text = "Menu";
         // 
         // toolStripMenuItem1
         // 
         toolStripMenuItem1.Name = "toolStripMenuItem1";
-        toolStripMenuItem1.Size = new Size(152, 22);
+        toolStripMenuItem1.Size = new Size(177, 24);
         toolStripMenuItem1.Text = "Activate MH";
         toolStripMenuItem1.Click += ToolStripMenuActivate_Click;
         // 
         // toolStripMenuItem2
         // 
         toolStripMenuItem2.Name = "toolStripMenuItem2";
-        toolStripMenuItem2.Size = new Size(152, 22);
+        toolStripMenuItem2.Size = new Size(177, 24);
         toolStripMenuItem2.Text = "Deactivate MH";
         toolStripMenuItem2.Click += ToolStripMenuDeactivate_Click;
         // 
         // closeToolStripMenuItem
         // 
         closeToolStripMenuItem.Name = "closeToolStripMenuItem";
-        closeToolStripMenuItem.Size = new Size(152, 22);
+        closeToolStripMenuItem.Size = new Size(177, 24);
         closeToolStripMenuItem.Text = "Exit";
         closeToolStripMenuItem.Click += ToolStripMenuExit_Click;
         // 
         // MainForm
         // 
-        AutoScaleDimensions = new SizeF(7F, 15F);
+        AutoScaleDimensions = new SizeF(8F, 20F);
         AutoScaleMode = AutoScaleMode.Font;
-        ClientSize = new Size(271, 98);
+        ClientSize = new Size(343, 140);
         Controls.Add(tableLayoutPanel1);
         Controls.Add(flowLayoutPanel1);
         FormBorderStyle = FormBorderStyle.FixedSingle;
+        Margin = new Padding(3, 4, 3, 4);
         MaximizeBox = false;
         Name = "MainForm";
         ShowIcon = false;
